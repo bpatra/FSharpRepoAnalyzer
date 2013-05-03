cec3b874b15a9b240690c14139c243d161668717
unknown <bpatr@EFRONT-0-00199.frontlabs.com>
2013-05-03 18:23:17 +0200

    Visual studio version is 2010, add group authors by commits and avg LinesAdd/LinesDeleted

--- RepoAnalysis/RepoManipulation.fs
+++ RepoAnalysis/RepoManipulation.fs
@@ -3,20 +3,59 @@ open LibGit2Sharp
 
 module RepoManipulation =
 
-type RepoManager(path:string) = 
-    member this.GetRepo() =
-        new Repository(path) //TODO: implement IDisposable...
+    type public RepoManager(path:string) = 
+        member this.GetRepo() =
+            new Repository(path) //TODO: implement IDisposable...
 
     
+    let minus x = -x
     
-let authorsOrderedByCommitsCount (repo:RepoManager) maxNumberOfCommits = 
-    repo.GetRepo().Commits
-    |> Seq.take maxNumberOfCommits 
-    |> Seq.groupBy (fun(commit) -> commit.Author)
-    |> Seq.sortBy (fun(group) -> 
-                        snd group
-                        |> Seq.length
-                    )
-                    
+    let seqSafeTakeFilter maxNumbers sequencesToTake=  match maxNumbers with
+                                                            | Some(maxNumbers) -> Seq.take maxNumbers sequencesToTake
+                                                            | None -> sequencesToTake
+    let authorsGrouped (repo:RepoManager) (maxNumberOfCommits:option<int>) = 
+        let seqSafeTake = seqSafeTakeFilter maxNumberOfCommits
+        repo.GetRepo().Commits
+        |> seqSafeTake 
+        |> Seq.groupBy (fun(commit) -> commit.Author.Name)
+       
+
+    let authorsOrderedByCommitsCount (repo:RepoManager) (maxNumberOfCommits:option<int>) = 
+        authorsGrouped repo maxNumberOfCommits
+        |> Seq.sortBy (fun(group) -> 
+                            snd group
+                            |> Seq.length
+                            |> minus
+                        )
+         |> Seq.head
+         |> fst
+
+    let parentLinearHistory (commit:Commit)= 
+        match Seq.isEmpty commit.Parents with
+            | true -> option<Commit>.None
+            | _ -> Some(Seq.head commit.Parents)
+
+    let authorsOrderedByAvgLinesAddedLinesDeleted (repo:RepoManager) (maxNumberOfCommits:option<int>) =  
+        let libGit2SharpRepo = repo.GetRepo()
+        
+        authorsGrouped repo maxNumberOfCommits
+        |> Seq.sortBy (fun(group) -> 
+                            snd group
+                            |> Seq.filter  (fun(commit) -> 
+                                                let parent = parentLinearHistory commit
+                                                match parent with
+                                                | None -> false
+                                                | _ -> true
+                                            )
+                            |> Seq.averageBy (fun(commit) -> 
+                                                let parent = parentLinearHistory commit
+                                                let diff = libGit2SharpRepo.Diff.Compare(parent.Value.Tree, commit.Tree)
+                                                match diff.LinesDeleted with
+                                                | 0 -> 1.0
+                                                | _ -> (float) (diff.LinesAdded /diff.LinesDeleted)
+                                            )
+
+                        )
+
 
                     
--- RepoAnalysisTests/Library1Tests.fs
+++ RepoAnalysisTests/Library1Tests.fs
@@ -13,8 +13,17 @@ open RepoAnalysis
 
 [<TestFixture>] 
 type ``Create a class instance of MyFirst Class`` ()=
-    let lightBulb = new MyFirstClass()
+    let repo = new RepoManipulation.RepoManager(@"C:repo1")
+
 
     [<Test>] member x.
-     ``Test member value.`` ()=
-            lightBulb.X |> should equal "Hello world"
+     ``First Commiter.`` ()=
+                RepoManipulation.authorsOrderedByCommitsCount repo option.None
+                      |> should equal "toto"
+            
+            
+       [<Test>] member x.
+     ``First CommiterAverageLinesAddedDelete.`` ()=
+                RepoManipulation.authorsOrderedByAvgLinesAddedLinesDeleted repo (Some(1000))
+                      |> should equal "oim"
+             
